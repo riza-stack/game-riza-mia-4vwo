de8d20db057b1ad36aa416540d18264cbd28d6cd
rizastack <>
2021-06-02 11:48:45 +0000

    woordenlijst gemaakt

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import random
 
+WoordenLijst=['informatica', 'informatiekunde', 'spelletje','aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit','verzekering', 'universiteit']
+
 def start():
     p=5
     print("Leuk, we gaan galgje spelen!")
     print("Je mag %s keer fout raden" %(p))
-    print("%s"%(tekening(p)))
+    #print("%s"%(tekening(p)))
     return(p)
+
+
+def SpeelSpel():
+  #todo
+  aap = "noot"
+
+start()
+    
