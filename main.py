908b689b925334b4bc65e3b8e938bd66d7dc34fe
Mia Van <>
2021-06-05 17:04:47 +0000

    als letter goed laten weten aan speler, en als letter fout laten weten aan speler

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import random
 
-def SpeelSpel():
-    pogingen=start()
-  WoordenLijst=['informatica', 'informatiekunde', 'spelletje','aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit','verzekering', 'universiteit']
-
 def start():
     p=5
     print("Leuk, we gaan galgje spelen!")
@@ -26,6 +22,31 @@ def ToonWoord(l2):
         print(i, end=" ")
     print("")
 
+def SpeelSpel():
+    pogingen=start()
+    WoordenLijst=['informatica', 'informatiekunde', 'spelletje','aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit','verzekering', 'universiteit']
+    list1,list2=WillekeurigWoord(WoordenLijst)
+    ToonWoord(list2)
 
-while pogingen > 0:
+    while pogingen > 0:
         letter = input("raad een letter:\n")
+        if letter in list1:
+            print('goed geraden!')
+            index=-1
+            for i in list1:
+                index+=1
+                if i==letter:
+                    list2[index]=letter
+        elif letter not in list1:
+            print("%s"%(tekening(pogingen-1)))
+            pogingen-=1
+            if pogingen==0:
+                print("jammer, je hangt!")
+                print('het woord was:')
+                ToonWoord(list1)
+            elif pogingen>0:
+                print("verkeerd geraden!, je mag nog %s keer fout raden"%(pogingen))
+        ToonWoord(list2)
+        if "_" not in list2:
+            print("je hebt het woord geraden!")
+            break
